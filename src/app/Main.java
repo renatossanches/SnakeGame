28cdfac41f5f246c6ae25682835fb79a627e13c0
renatossanches <renato.sanches2004@gmail.com>
2025-05-20 17:54:58 -0300

    Feature - creating Main

---
+++ src/app/Main.java
@@ -0,0 +1,24 @@
+package app;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/view/gui.fxml"));
+		stage.setTitle("Snake game");
+		stage.setScene(new Scene(root));
+		stage.show();
+		
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
